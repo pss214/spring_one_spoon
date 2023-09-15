5bbccc204ed9d4790cd17b90812f0edb0f35859e
pss214 <bak10175@gmail.com>
2023-09-15 10:13:50 +0900

    two_week uplode

---
+++ two_week/Lion.java
@@ -0,0 +1,16 @@
+package two_week;
+
+public class Lion extends Animal{
+
+    public Lion(String name) {
+        super(name);
+    }
+
+    @Override
+    public void eat(){
+        System.out.println(name+"가 씹어 먹다");
+    }
+    public void run(){
+        System.out.println(name+"가 달리다");
+    }
+}
---
+++ two_week/MyInfoMain.java
@@ -0,0 +1,13 @@
+package two_week;
+
+public class MyInfoMain {
+    public static void main(String[] args) {
+        MyInfo myInfo = new MyInfo();
+        myInfo.setName("홍길동");
+        myInfo.setAge(24);
+        myInfo.setStudent_id("20073005");
+        System.out.println(myInfo.getName()+myInfo.getAge()+
+                myInfo.getSchool_name()+ myInfo.getStudent_id());
+        myInfo.printAll();
+    }
+}
